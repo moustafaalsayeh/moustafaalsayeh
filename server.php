c43fb5fb1ad506889b5c554e40fa01b26e59be06
Moustafa Alsayeh <elsayhsoft@gmail.com>
2022-07-27 18:53:09 +0200

    Update server.php

--- server.php
+++ server.php
@@ -1,16 +1,31 @@
-<?php
-
-/*
-  * Server
-  */
-  class TestSoapServer
-  {
-      public function getMessage()
-      {
-          return 'Hello, World!';
-      }
-  }
-  $options = ['uri' => 'http://localhost/'];
-  $server = new SoapServer(null, $options);
-  $server->setClass('TestSoapServer');
+<?php
+
+
+
+/*
+
+  * Server
+
+  */
+
+  class TestSoapServer
+
+  {
+
+      public function getMessage()
+
+      {
+
+          return 'Hello, World!';
+
+      }
+
+  }
+
+  $options = ['uri' => 'http://domainname.com/'];
+
+  $server = new SoapServer(null, $options);
+
+  $server->setClass('TestSoapServer');
+
   $server->handle();
